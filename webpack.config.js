aebee7bcdd3ca3f86f12b10078f73526cc6fe2e8
Gepoverlow <gepeoverload@gmail.com>
2021-11-07 18:11:23 +0100

    changed development to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   // To debug more efficiently:
   devtool: "inline-source-map",
